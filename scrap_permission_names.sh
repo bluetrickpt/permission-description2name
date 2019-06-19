ced60690e4efb5ced5f8f6e9e3fc48dbe0b1548b
Ricardo Mendes <rscmendes@protonmail.com>
2019-06-19 12:55:29 +0100

    Adds outputs folder

--- scrap_permission_names.sh
+++ scrap_permission_names.sh
@@ -2,11 +2,12 @@
 
 BASE_URL="https://developer.android.com/reference/android/Manifest.permission"
 MANIFEST_PAGE_FILE="manifest.html"
+OUTPUT_PATH="outputs/"
 
 wget $BASE_URL -q -O $MANIFEST_PAGE_FILE
 
 cat $MANIFEST_PAGE_FILE | grep -o \
     -e "\"android.*permission.*\"" \
-    -e "\"com.android.*.permission.*\"" | grep -v "android.Manifest.permission" | tr -d '"' > permission_names
+    -e "\"com.android.*.permission.*\"" | grep -v "android.Manifest.permission" | tr -d '"' > ${OUTPUT_PATH}permission_names
 
 rm $MANIFEST_PAGE_FILE
