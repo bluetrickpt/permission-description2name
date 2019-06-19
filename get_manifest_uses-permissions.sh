1bbd8624e56724621f36b1edf15af64fb9b5962f
Ricardo Mendes <rscmendes@protonmail.com>
2019-06-19 14:36:12 +0100

    Adds script to write permissions in uses-permission manifest format

---
+++ get_manifest_uses-permissions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+OUTPUT_PATH="outputs/"
+PERMISSION_NAME_FILE="${OUTPUT_PATH}permission_names"
+OUTPUT_FILE="${OUTPUT_PATH}permission_manifest"
+
+> $OUTPUT_FILE  # truncate
+
+while read PERMISSION; do
+    echo "<uses-permission android:name=\"${PERMISSION}\"/>" >> $OUTPUT_FILE
+done < $PERMISSION_NAME_FILE
+
--- scrap_permission_names.sh
+++ scrap_permission_names.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+OUTPUT_PATH="outputs/"
+OUTPUT_FILE="${OUTPUT_PATH}permission_names"
+
 BASE_URL="https://developer.android.com/reference/android/Manifest.permission"
 MANIFEST_PAGE_FILE="manifest.html"
-OUTPUT_PATH="outputs/"
 
 wget $BASE_URL -q -O $MANIFEST_PAGE_FILE
 
 cat $MANIFEST_PAGE_FILE | grep -o \
     -e "\"android.*permission.*\"" \
-    -e "\"com.android.*.permission.*\"" | grep -v "android.Manifest.permission" | tr -d '"' > ${OUTPUT_PATH}permission_names
+    -e "\"com.android.*.permission.*\"" | grep -v "android.Manifest.permission" | tr -d '"' > ${OUTPUT_FILE}
 
 rm $MANIFEST_PAGE_FILE
